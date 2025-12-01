2037469a8b731f230e518f8cf0d57eca6818c0c0
Yinan Xu <xuyinan@ict.ac.cn>
2025-12-01 13:51:59 +0800

    Update build and run scripts for litmus-tests-riscv

--- workloads/linux/litmus-tests-riscv/build.sh
+++ workloads/linux/litmus-tests-riscv/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-unzip -q -d "$SRC_DIR" "$SRC_DIR/litmus-test-riscv-master.zip"
-eval $(opam env)
-make -C "$SRC_DIR/litmus-tests-riscv-master" hw-tests CORES=2 GCC=riscv64-unknown-linux-gnu-gcc -j8
-cp -r "$SRC_DIR/litmus-tests-riscv-master/hw-tests" "$PKG_DIR/litmus-tests-riscv"
-install -m 755 "$WORKLOAD_DIR/run.sh" "$PKG_DIR/litmus-tests-riscv/run-own.sh"
+tar -C "$SRC_DIR" -xzf "$SRC_DIR/litmus-tests-riscv-2C.tar.gz"
+cp -r "$SRC_DIR/litmus-tests-riscv-2C" "$PKG_DIR/litmus-tests-riscv"
+install -m 755 "$WORKLOAD_DIR/run.sh" "$PKG_DIR/litmus-tests-riscv/run-simple.sh"
 install -Dm 644 "$WORKLOAD_DIR/inittab" "$PKG_DIR/etc/inittab"
